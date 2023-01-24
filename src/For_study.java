40110c12945e3d5b4bc43e1508800633501ad558
DPfeyfer <Skydenfer737@gmail.com>
2023-01-24 10:17:21 +0300

    First commit

---
+++ src/For_study.java
@@ -0,0 +1,14 @@
+public class For_study {
+    public static void main(String[] args) {Lesson_1(); Lesson_2(); Lesson_3();
+    }
+
+    public static void Lesson_1() {
+    }
+
+    public static void Lesson_2() {
+    }
+
+    public static void Lesson_3() {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {Task_1(); Task_2(); Task_3();
+    }
+
+    public static void Task_1() {
+        System.out.println("Task 1");
+        System.out.println();
+    }
+
+    public static void Task_2() {
+        System.out.println("Task 2");
+        System.out.println();
+    }
+
+    public static void Task_3() {
+        System.out.println("Task 3");
+        System.out.println();
+
+    }
+}
