99a0bf4c924309aa462d98bc5162b1379aaa4192
DPfeyfer <Skydenfer737@gmail.com>
2023-01-24 14:04:07 +0300

    Task 1 complete

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.security.cert.CertificateNotYetValidException;
+
 public class Main {
     public static void main(String[] args) {Task_1(); Task_2(); Task_3();
     }
 
     public static void Task_1() {
         System.out.println("Task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+//        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+//        System.out.println("Full name employee is " + fullName);
+        System.out.println("Full name employee is " + lastName + ' ' + firstName + ' ' + middleName);
         System.out.println();
     }
 
